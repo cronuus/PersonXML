91997517626fade7b384fbe5821b48a0f86768ae
Ghukas Buniatyan <g.buniatyan09@gmail.com>
2022-07-01 17:42:09 +0400

    PersonXML

---
+++ PersonXML/PersonXML/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PersonXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xml = XDocument.Load("XMLFile1.xml");
+            var persons = xml.Descendants("person");
+            var teachers = persons.Select(person => person.Elements("teacher"));
+            var qualificationsContainer = teachers.SelectMany(teacher => teacher.Elements("qualifications"));
+            var qualifications = qualificationsContainer.SelectMany(qualificationContainer => qualificationContainer.Elements("qualification"));
+            var qualificationIds = qualifications.Select(qualification => qualification.Element("id").Value);
+        }
+    }
+}
